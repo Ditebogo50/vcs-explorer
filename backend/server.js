ae7272d7f1a498fdd7b6ffb5e7d5eea9ea86508c
Suzan <sue7led@gmail.com>
2023-09-01 11:09:58 +0200

    Add github user search

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,38 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet')
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const axios = require("axios");
 
 const app = express();
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
 
 const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Backend!');
+app.get("/api/search", async (req, res) => {
+  try {
+    const username = req.query.username;
+
+    // Search in GitHub
+    const gitHubUsersResponse = await axios.get(
+      `https://api.github.com/search/users?q=${username}`
+    );
+
+    const gitHubUsers = gitHubUsersResponse.data.items.map((user) => (
+      { 
+        provider: 'github',
+        id: user.id, 
+        username: user.login 
+      }
+    ));
+
+    const allUsers = [...gitHubUsers];
+    res.json({ users: allUsers });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred" });
+  }
 });
 
 app.listen(port, () => {
